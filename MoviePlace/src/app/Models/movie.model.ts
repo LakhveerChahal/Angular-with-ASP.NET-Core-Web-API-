56a694570dfa41f650849d25b413c592c0cfabf6
Lakhveer Singh <luckychahal4@gmail.com>
2020-07-08 10:09:57 +0530

    Initial commit

--- MoviePlace/src/app/Models/movie.model.ts
+++ MoviePlace/src/app/Models/movie.model.ts
@@ -3,7 +3,7 @@ import { Genre } from './genre.model';
 export interface Movie{
     movieId: string;
     movieName: string;
-    movieGenres: string[];
     moviePrice: number;
+    genres: Genre[];
     url: string;
 }
